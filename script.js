924fc9879a4b96d131ae7087222753affe9674c6
Marc Wright <marc@generalassemb.ly>
2021-03-17 15:43:50 -0500

    top row win condition working

--- script.js
+++ script.js
@@ -4,14 +4,26 @@ const squares = document.querySelectorAll('#board > div')
 let turnCount = 0
 let turn = 'X'
 
+console.log(moves)
+function checkForWin() {
+    if (moves[0] === moves[1] && moves[1] === moves[2]) {
+        alert('winner in the first row ' + turn)
+    } else {
+        console.log('no winner yet')
+    }
+}
+
 squares.forEach((square, index) => {
     square.addEventListener('click', (event)=> {
         moves[index] = turn
-        console.log(moves)
+        checkForWin()
+
         event.target.innerText = turn
 
         turn = (turnCount % 2 === 0) ? "O" : "X";
 
         turnCount++
+
     }, {once: true})
 })
+
