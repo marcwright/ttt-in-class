4fdd86a4d8465ddb27fe45275158e291c8df54c7
Marc Wright <marc@generalassemb.ly>
2021-03-17 16:02:48 -0500

    remove event listener

--- script.js
+++ script.js
@@ -4,26 +4,31 @@ const squares = document.querySelectorAll('#board > div')
 let turnCount = 0
 let turn = 'X'
 
-console.log(moves)
 function checkForWin() {
     if (moves[0] === moves[1] && moves[1] === moves[2]) {
+        removeClickEventCallback()
         alert('winner in the first row ' + turn)
     } else {
         console.log('no winner yet')
     }
 }
 
+function removeClickEventCallback() {
+    squares.forEach(square =>
+        square.removeEventListener('click', handleClickEventCallback)
+    )
+}
+
+function handleClickEventCallback(event) {
+    moves[event.target.id] = turn
+    checkForWin()
+    event.target.innerText = turn
+    turn = (turnCount % 2 === 0) ? "O" : "X";
+    turnCount++
+}
+
 squares.forEach((square, index) => {
-    square.addEventListener('click', (event)=> {
-        moves[index] = turn
-        checkForWin()
-
-        event.target.innerText = turn
-
-        turn = (turnCount % 2 === 0) ? "O" : "X";
-
-        turnCount++
-
-    }, {once: true})
+    square.setAttribute('id', `${index}`)
+    square.addEventListener('click', handleClickEventCallback, {once: true})
 })
 
